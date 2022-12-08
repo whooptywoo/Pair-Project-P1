5d523df917fac977b46a2a91cf7d9b9cbc07b3eb
melissarsy <melissamey2712@gmail.com>
2022-12-08 15:16:13 +0700

    update login dan register

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
-const {Course} = require('../models')
+const { Course, User} = require('../models')
+const bcrypt = require('bcryptjs')
 
 class Controller {
     static home (req, res) {
@@ -20,6 +21,45 @@ class Controller {
             .then(course => res.render('courseDetail', {course}))
             .catch(err => res.send(err))
     }
+    
+    static registerForm(req, res){
+        res.render('register')
+      }
+    
+      static register(req, res){
+        console.log(req.body)
+        const data = req.body
+        User.create({
+          email: data.email, 
+          password: data.password,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+          .then(user => res.redirect('/login'))
+          .catch(err => res.send(err))
+      }
+    
+      static loginform(req, res){
+        res.render('login')
+      }
+    
+      static login(req, res){
+        const {email, password} = req.body
+    
+        User.findOne({where: { email }})
+          .then(user => {
+            if(user){
+              const isValidPassword = bcrypt.compareSync(password, user.password)
+    
+              if(isValidPassword){
+                res.redirect(`/${user.id}`)
+              } else {
+                res.send('gagal login')
+              }
+            }
+          })
+          .catch(err => res.send(err))
+      }
+    
 }
-
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,16 @@ const router = express.Router();
 const Controller = require('../controllers/');
 
 
+
 router.get('/', Controller.home);
 router.get('/:courseId/courseDetail', Controller.courseDetail)
 // router.get('/:id')
 // router.use('/courseDetail', require());
+
+router.get('/register', Controller.registerForm)
+router.post('/register', Controller.register)
+
+router.get('/login', Controller.loginform)
+router.post('/login', Controller.login)
 
 module.exports = router;
